3d7ce9b9c8d61f926566da4d87dbd70d556193cf
Marcelo E. Cardoso <marc.cardo@gmail.com>
2014-04-07 15:29:10 -0300

    Sets default locales to pt-BR

--- config/initializers/refinery/i18n.rb
+++ config/initializers/refinery/i18n.rb
@@ -1,11 +1,11 @@
 # encoding: utf-8
 
 Refinery::I18n.configure do |config|
-  # config.default_locale = :en
+  config.default_locale = :'pt-BR'
 
-  # config.current_locale = :en
+  config.current_locale = :'pt-BR'
 
-  # config.default_frontend_locale = :en
+  config.default_frontend_locale = :'pt-BR'  
 
   # config.frontend_locales = [:en]
 
