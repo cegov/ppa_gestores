d2eb4e294f8c89fe1e34a0f6a3546bd6a0bcaa9e
Marcelo E. Cardoso <marc.cardo@gmail.com>
2014-04-08 18:56:48 -0300

    Tries to remove precompile flag. With the database on, this should work

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,7 @@ module PpaGestores
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = false # for Heroku
+    # config.assets.initialize_on_precompile = false # for Heroku
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
