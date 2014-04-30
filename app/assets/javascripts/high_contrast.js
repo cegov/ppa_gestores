eb91a3064b284ffb79ca8d02d321f0a6257a48f0
Marcelo E. Cardoso <marc.cardo@gmail.com>
2014-04-30 14:00:31 -0300

    Fixes bug that prevented editing and saving pt-BR pages created by the user

--- app/assets/javascripts/high_contrast.js
+++ app/assets/javascripts/high_contrast.js
@@ -1,19 +1,23 @@
+// This class is used to define how elements will behave in high contrast mode
+// to define the intended look do so in the file bootstrap_and_overrides.css.less
 var HighContrast = {
 	isOn: false,
 	objDefaults: {
 		initTarget: "#high-contrast-tgt",
+		
+		/**********************************************************************/
+		// ADD TO THE LIST PAGE ELEMENTS THAT SHOULD HAVE THEIR COLOR CHANGED IN 
+		// HIGH CONTRAST MODE
 		backgroundTargets: ['header', '#accessibility_menu', "#logo_bar", 
 			"#page .sidebar-nav", "#nav_services .navbar-inner", "body", 
 			"footer", "footer #government_links"
 		]
+		/**********************************************************************/
 	}, 
 	init: function(){
 		this.initAccessibilityButton();			
 	},
-	initAccessibilityButton: function(){
-		// toggle flag
-		
-		
+	initAccessibilityButton: function(){	
 		$(HighContrast.objDefaults.initTarget).on('click', function(){
 			if(HighContrast.isOn == false){
 				HighContrast.isOn = true;
@@ -27,9 +31,7 @@ var HighContrast = {
 				$.each(HighContrast.objDefaults.backgroundTargets, function( index, idOrClassOfTheElement ) {
 					$(idOrClassOfTheElement).removeClass("high-contrast");
 				});
-			}
-			
-			console.log("hello!");
+			}			
 		});
 	}
 	
