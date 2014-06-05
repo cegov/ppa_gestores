1faf4ac7c55be2c2667cf2d25033a382fb2beabf
Marcelo E. Cardoso <marc.cardo@gmail.com>
2014-06-05 14:51:13 -0300

    Adds pagination to courses#index view

--- vendor/extensions/courses/app/controllers/refinery/courses/courses_controller.rb
+++ vendor/extensions/courses/app/controllers/refinery/courses/courses_controller.rb
@@ -6,6 +6,8 @@ module Refinery
       before_filter :find_page
       before_filter :find_all_newsarticles  
       
+      MAX_COURSES_PER_PAGE = 20
+      
       def index
         # you can use meta fields from your model instead (e.g. browser_title)
         # by swapping @page for @course in the line below:
@@ -27,7 +29,7 @@ module Refinery
       
       
       def find_all_courses
-        @courses = Course.order('position ASC')
+        @courses = Course.order('position ASC').page(params[:page]).per_page(MAX_COURSES_PER_PAGE)
       end
 
       def find_page
