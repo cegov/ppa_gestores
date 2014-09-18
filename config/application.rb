b3e2dd459495f61824a5ee6606896df55cb6809c
Marcelo E. Cardoso <marc.cardo@gmail.com>
2014-09-18 12:12:13 -0300

    Tries to solve problem of custom modules not loading

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module PpaGestores
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += Dir["#{config.root}/vendor/extensions/**"]  # I invented this
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -63,6 +63,7 @@ module PpaGestores
     # Enable the asset pipeline
     config.assets.enabled = true
     # config.assets.initialize_on_precompile = false # for Heroku
+    config.assets.initialize_on_precompile = true # changed after not loading extensions...
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
