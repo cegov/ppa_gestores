3d51b205aa7feaa0f5f53ea3a3205d09e2ae9761
Marcelo E. Cardoso <marc.cardo@gmail.com>
2014-09-18 13:43:52 -0300

    Tries to revert to perfect configuration, written before celentes voodoo

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module PpaGestores
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += Dir["#{config.root}/vendor/extensions/**"]  # I invented this
+    # config.autoload_paths += Dir["#{config.root}/vendor/extensions/**"]  # I invented this
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,11 +2,11 @@ PpaGestores::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
-  config.cache_classes = false # changed on 18/09
+  config.cache_classes = true # changed on 18/09
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = false # changed on 18/09
+  config.action_controller.perform_caching = true # changed on 18/09
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
