36edb6f54b5189e8be88e9529bde9ce1b4b63666
Marcelo E. Cardoso <marc.cardo@gmail.com>
2014-12-08 16:15:51 -0200

    Adds html sitemap functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery except: :verify_user
   before_filter :get_courses
   
+  helper_method :refinery_user?
+  
+  
   def verify_user
     username = params['refinery_user']['username']
     password = params['refinery_user']['password']
@@ -11,6 +14,19 @@ class ApplicationController < ActionController::Base
     else 
       head :bad_request
     end
+  end
+  
+  def get_sitemap
+    @page = nil # ::Refinery::Page.where(:link_url => "/newsarticles").first # gambiarra para definir o template usado
+    @newsarticles = Refinery::Newsarticles::Newsarticle.get_published.last(30)
+    @courses_for_sitemap = Refinery::Courses::Course.where(:is_active => true)
+    @pages = Refinery::Page.where show_in_menu: true    
+    respond_to do |format|
+      format.html {       
+        render "sitemap/sitemap", layout: true
+      }
+      format.xml { render "sitemap/index.xml" }
+    end        
   end
   
   def get_courses
